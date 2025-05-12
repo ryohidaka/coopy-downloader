e128ed081a69acce35ee6c25d427a76cf38627fe
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-12 20:32:00 +0900

    feat: 説明文を変更

--- cmd/root.go
+++ cmd/root.go
@@ -30,13 +30,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "coopy-downloader",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "コープこうべの宅配の注文書(CSV)をダウンロードするCLIツール",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
