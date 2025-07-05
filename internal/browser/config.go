3b16dc0ce03cb09ce810e1a1207bf4d48ffe9371
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-07-05 20:26:57 +0900

    fix: タイムアウト時間を修正

--- internal/browser/config.go
+++ internal/browser/config.go
@@ -21,8 +21,8 @@ func CreateChromedpContext(noSandbox bool) (context.Context, context.CancelFunc)
 	// Create allocator context
 	allocCtx, allocCancel := chromedp.NewExecAllocator(context.Background(), opts...)
 
-	// Set timeout to 30 seconds
-	timeoutCtx, timeoutCancel := context.WithTimeout(allocCtx, 60*time.Second)
+	// Set timeout to 3 minute
+	timeoutCtx, timeoutCancel := context.WithTimeout(allocCtx, 3*time.Minute)
 
 	// Create chromedp context with timeout
 	ctx, cancelCtx := chromedp.NewContext(timeoutCtx)
